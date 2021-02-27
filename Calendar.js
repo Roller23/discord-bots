db4451f40a8d3ed38b7adc98d87602e6e79420e0
Roller23 <roller47@wp.pl>
2021-02-27 13:45:30 +0100

    connecting to db

--- Calendar.js
+++ Calendar.js
@@ -16,7 +16,6 @@ module.exports = {
   guildID: '592409592315772938',
   connectToDb() {
     const uri = `mongodb+srv://domopedia:${process.env.MONGO_PASSWORD}@bots-cluster.k06nu.mongodb.net/domopedia?retryWrites=true&w=majority`;
-    console.log('pw', process.env.MONGO_PASSWORD);
     const self = this;
     self.client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
     return new Promise((resolve, reject) => {
