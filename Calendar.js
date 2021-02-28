b08d31ab1d0849e4d8a2acfa6ed964186f2cb463
Roller23 <roller47@wp.pl>
2021-02-28 01:09:48 +0100

    testing notifying

--- Calendar.js
+++ Calendar.js
@@ -121,7 +121,9 @@ module.exports = {
             tomorrow.setDate(tomorrow.getDate() + 1);
             let minutesPassed = Math.floor((tomorrow.getTime() - event.date.getTime()) / 1000 / 60);
             let hoursPassed = Math.floor(minutesPassed / 60);
+            console.log('minutes', minutesPassed, 'hours', hoursPassed);
             if (!event.notifiedDayBefore && hoursPassed === 23) {
+              console.log('notifying')
               let guild = self.getGuild(self.slaves[i]);
               let channel = guild.channels.cache.get(self.channelID);
               channel.send(`@everyone wakey wakey: ${self.createEventEmbed(event)}`);
