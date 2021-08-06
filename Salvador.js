dab170625be85b0b6bf31f2b7c98d22fa889fb1c
Wiktor Kania <roller47@wp.pl>
2021-08-06 11:24:32 +0200

    Added pinging Hermes

--- Salvador.js
+++ Salvador.js
@@ -4,6 +4,8 @@ const fetch = require('node-fetch')
 module.exports = {
   heroku: 'https://pomodoro-salvadoro.herokuapp.com',
   client: new Discord.Client(),
+  lastHermesPing: 0,
+  hermesWakeEndpoint: 'https://raingo.herokuapp.com/api/wake',
   run(TOKEN) {
     let self = this;
     let client = this.client;
@@ -12,6 +14,19 @@ module.exports = {
       client.user.setActivity('Teach me anime senpai', {type: 'PLAYING'});
     });
     client.on('message', async msg => {
+      const now = Date.now();
+      const wakeDiff = now - this.lastHermesPing;
+      const diffHours = wakeDiff / 1000 / 60 / 60;
+      if (diffHours >= 1) {
+        this.lastHermesPing = now;
+        await fetch(this.hermesWakeEndpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({msg: msg.content, user: msg.author.username})
+        })
+      }
       if (msg.content.toLowerCase() === 'gimme image') {
         msg.reply("I'm on it");
         try {
